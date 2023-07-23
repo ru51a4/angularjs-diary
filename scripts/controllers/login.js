3829c62e7344cad54d158dde191d698c13b096e6
ru51a4 <rruslanvaleev@yandex.ru>
2023-07-23 18:49:02 +0500

    resolve

--- scripts/controllers/login.js
+++ scripts/controllers/login.js
@@ -4,12 +4,7 @@ $app
     .controller('LoginCtrl', function ($scope, $http, $location, api) {
         $scope.email = "";
         $scope.password = "";
-        let init = () => {
-            api.checkAuth();
-        };
-        init();
         $scope.login = () => {
-            console.log($scope.email, $scope.password)
             api.login({ email: $scope.email, password: $scope.password }).then(function successCallback(response) {
                 if (response.data.token) {
                     localStorage.setItem("jwt", response.data.token);
